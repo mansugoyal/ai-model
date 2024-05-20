9192ccd3cb50baaee55591ab6984af91db2fbe83
mansugoyal <mansugoyal1234@gmail.com>
2024-05-20 22:00:40 +0530

    added screenshot cmd and wikipedia info cmd

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import pyaudio
 import pywhatkit
+import pyautogui
 from gtts import gTTS
 from playsound import playsound
 
@@ -30,6 +31,25 @@ text = get_audio()
 if "youtube" in text.lower():
     speech(f"Okay, i will bring {text} for you")
     pywhatkit.playonyt(text)
+elif "info" in text.lower():
+    try:
+        print("Attempting to fetch info...")
+        info = pywhatkit.info(text, lines=5)
+        print("Info fetched successfully.")
+        print(info)
+        speech(info)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        speech("Sorry, I couldn't fetch the information.")
+elif "screenshot" in text.lower():
+      # Take a screenshot
+    try:
+        screenshot = pyautogui.screenshot()
+        screenshot.save("myscreenshot.png")
+        speech("Screenshot taken and saved as myscreenshot.png")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        speech("Sorry, I couldn't take the screenshot.")
 elif "joke" in text.lower():
     speech("What does a storm cloud wear under his raincoat? Thunderwear.")
 else:
